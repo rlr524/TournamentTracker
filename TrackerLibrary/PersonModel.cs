84fbd881362d83f65210ead988c01a87122f7c31
Rob Ranf <rob@emiyaconsulting.com>
2022-05-15 17:51:49 -0700

    init

---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TournamentTracker.TrackerLibrary
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string MobilePhoneNumber { get; set; }
+    }
+}
